b535cfbe231564a5115a30273abfddfdd4dfc3b4
Louis Verret <louis@extrality.ai>
2023-12-01 20:54:12 +0100

    add day1

---
+++ 2023/day1.py
@@ -0,0 +1,16 @@
+import re
+
+def read_txt(filename):
+    return [line.rstrip('\n') for line in open(filename, 'r')]
+
+def part1(l):
+    return sum((lambda x: int(x[0]+x[-1]))(re.findall("\d", s)) for s in l)
+
+def part2(l):
+    d = {"zer": "o0", "on": "e1", "tw": "o2", "thre": "e3", "fou": "r4", "fiv": "e5", "si": "x6", "seve": "n7", "eigh": "t8", "nin": "e9"}
+    return part1([re.compile("|".join(map(lambda x: f"{x}(?={d[x][0]})", list(d)))).sub(lambda x: d[x.group()][1], s) for s in l])
+
+if __name__ == "__main__":
+    lines = read_txt("day1.txt")
+    print(part1(lines))
+    print(part2(lines))
