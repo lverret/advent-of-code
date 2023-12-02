aa1b0fefc7754412e3f286206591fc7a3dfb18b4
Louis Verret <louis@extrality.ai>
2023-12-02 13:08:10 +0100

    reduce day1

--- 2023/day1.py
+++ 2023/day1.py
@@ -9,7 +9,7 @@ def part1(l):
 
 def part2(l):
     d = {"zer": "o0", "on": "e1", "tw": "o2", "thre": "e3", "fou": "r4", "fiv": "e5", "si": "x6", "seve": "n7", "eigh": "t8", "nin": "e9"}
-    return part1([re.compile("|".join(map(lambda x: f"{x}(?={d[x][0]})", list(d)))).sub(lambda x: d[x.group()][1], s) for s in l])
+    return part1([re.compile("|".join(f"{x}(?={d[x][0]})" for x in d)).sub(lambda x: d[x.group()][1], s) for s in l])
 
 
 if __name__ == "__main__":
